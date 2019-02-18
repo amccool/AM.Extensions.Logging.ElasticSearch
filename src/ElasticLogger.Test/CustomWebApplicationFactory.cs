bd23c3b2482da380a9c407267791f2c8d48fb05c
Brian Stauff <brian.stauff.github@gmail.com>
2019-02-18 15:51:09 -0600

    Change provider category evaluation to match most similar category

---
+++ src/ElasticLogger.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using AM.Extensions.Logging.ElasticSearch;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ElasticLogger.Test
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup: class
+    {
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            return WebHost.CreateDefaultBuilder()
+                .UseStartup<TStartup>();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseContentRoot(".");
+
+            builder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("diagnostics.json");
+            });
+
+            builder.ConfigureLogging((hostingContext, logging) =>
+            {
+                Uri uri = new Uri(@"http://es.fakething.net:9200");
+
+                logging.AddElasticSearch(options => { options.ElasticsearchEndpoint = uri; });
+            });
+
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
