832d25f01a738a5efcca4033b67b5935cfaff367
Alex McCool <alex.mccool@yahoo.com>
2019-05-16 12:59:59 -0700

    better integration tests change the provider to do less

---
+++ src/ElasticLogger.Test/Tweet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElasticLogger.Test
+{
+    internal class Tweet
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public DateTime PostDate { get; set; }
+        public string Message { get; set; }
+    }
+}
--- src/ElasticSearch.Extensions.Logging/ElasticsearchLoggingFactoryExtensions.cs
+++ src/ElasticSearch.Extensions.Logging/ElasticsearchLoggingFactoryExtensions.cs
@@ -1,30 +1,64 @@
-﻿namespace AM.Extensions.Logging.ElasticSearch
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AM.Extensions.Logging.ElasticSearch
 {
     public static class ElasticsearchLoggingFactoryExtensions
     {
 
-        //#region logger factory
-        ///// <summary>
-        ///// Enable ElasticSearch as logging provider in .NET Core.
-        ///// </summary>
-        ///// <param name="factory"></param>
-        ///// <param name="endpoint">uri with ES port 9200 endpoint</param>
-        ///// <returns>ILoggerFactory for chaining</returns>
-        //public static ILoggerFactory AddElasticSearch(this ILoggerFactory factory, Uri endpoint)
-        //{
-        //    return AddElasticSearch(factory, endpoint, LogLevel.Trace, "trace");
-        //}
+        #region logger factory
+        /// <summary>
+        /// Enable ElasticSearch as logging provider in .NET Core.
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <param name="endpoint">uri with ES port 9200 endpoint</param>
+        /// <returns>ILoggerFactory for chaining</returns>
+        public static ILoggerFactory AddElasticSearch(this ILoggerFactory factory, Uri endpoint)
+        {
+            return AddElasticSearch(factory, new ElasticsearchLoggerOptions()
+            {
+                ElasticsearchEndpoint = endpoint,
+                IndexName = "trace"
+            });
+        }
+        public static ILoggerFactory AddElasticSearch(this ILoggerFactory factory, Uri endpoint, string indexPrefix)
+        {
+            return AddElasticSearch(factory, new ElasticsearchLoggerOptions()
+            {
+                ElasticsearchEndpoint = endpoint,
+                IndexName = indexPrefix
+            });
+        }
         //public static ILoggerFactory AddElasticSearch(this ILoggerFactory factory, Uri endpoint, LogLevel minLevel)
         //{
         //    return AddElasticSearch(factory, endpoint, (n, l) => l >= minLevel, "trace");
         //}
 
-        //public static ILoggerFactory AddElasticSearch(this ILoggerFactory factory, Uri endpoint, LogLevel filter, string indexPrefix = "trace")
+        //public static ILoggerFactory AddElasticSearch(this ILoggerFactory factory, Uri endpoint, LogLevel filter, string indexPrefix)
         //{
-        //    factory.AddProvider(new ElasticsearchLoggerProvider(endpoint, filter, indexPrefix));
+        //    var x = new OptionsMonitor< ElasticsearchLoggerOptions >(new OptionsFactory<ElasticsearchLoggerOptions>(), )
+
+
+
+        //    var o = new ElasticsearchLoggerOptions()
+        //    {
+        //        ElasticsearchEndpoint = endpoint,
+        //        IndexName = indexPrefix
+        //    };
+        //    factory.AddProvider(new ElasticsearchLoggerProvider(, 
+        //        endpoint, filter, indexPrefix));
         //    return factory;
         //}
-        //#endregion
+
+
+        public static ILoggerFactory AddElasticSearch(this ILoggerFactory loggerFactory, ElasticsearchLoggerOptions options)
+        {
+            loggerFactory.AddProvider(new ElasticsearchLoggerProvider(options));
+            return loggerFactory;
+        }
+
+        #endregion
 
     }
 }
