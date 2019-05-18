52bf4d8699efe686e64e91055c3dd95ad96e8b11
Brian Stauff <brian.stauff.github@gmail.com>
2019-05-18 10:41:33 -0500

    Fixing log levels (#17)
    * Adding .vscode to gitignore
    
    * Fixed bug with logger provider not recognizing config levels
    
    * Removing unused extension method

--- src/ElasticLogger.Test/ESLoggerTests.cs
+++ src/ElasticLogger.Test/ESLoggerTests.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AM.Extensions.Logging.ElasticSearch;
 using Elasticsearch.Net;
 using ElasticsearchInside;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 using Nest;
 using Newtonsoft.Json;
 using Xunit;
@@ -109,6 +114,5 @@ namespace ElasticLogger.Test
 
             await client.GetAsync("/");
         }
-
     }
 }
---
+++ src/ElasticLogger.Test/ElasticsearchLoggerFilterOptionsExtensionsTests.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using AM.Extensions.Logging.ElasticSearch;
+using System.Net;
+using System;
+using Microsoft.Extensions.Options;
+using Xunit;
+using Microsoft.Extensions.Configuration.Memory;
+using System.Collections.Generic;
+
+namespace ElasticLogger.Test
+{
+    public class ElasticsearchLoggerFilterOptionsExtensionsTests
+    {
+        [Fact]
+        public void No_categories_for_es_should_use_default()
+        {
+            var config = new ConfigurationBuilder()
+                            .Add(new MemoryConfigurationSource
+                            {
+                                InitialData = new Dictionary<string, string>
+                                {
+                                    {"Logging:Elasticsearch:IncludeScopes", "true" },
+                                    {"Logging:Elasticsearch:LogLevel:Default", "Error" },
+                                    {"Logging:Elasticsearch:LogLevel:My.Bananas.Trace", "Trace" },
+                                    {"Logging:LogLevel:Default", "Information" }
+                                }
+                            })
+                            .Build();
+
+            var serviceProvider = new ServiceCollection()
+                                        .AddLogging(x =>
+                                        {
+                                            x.AddConfiguration(config.GetSection("Logging"));
+                                            x.AddElasticSearch(options =>
+                                            {
+                                                options.ElasticsearchEndpoint = new Uri("http://bananas.net");
+                                                options.IndexName = "trace";
+                                            });
+                                        })
+                                        .BuildServiceProvider();
+
+            var loggerFilterOptions = serviceProvider.GetService<IOptions<LoggerFilterOptions>>();
+
+            ////This category doesn't exist in our diagnostics.json, so we should get the default (error)
+            //var retrievedLogLevel = loggerFilterOptions.GetLogLevelForCategoryName("logLevelThatDoesntExist");
+
+            //Assert.Equal(LogLevel.Error, retrievedLogLevel);
+        }
+
+        [Fact]
+        public void Should_use_log_level_of_matching_category_name()
+        {
+            var config = new ConfigurationBuilder()
+                            .Add(new MemoryConfigurationSource
+                            {
+                                InitialData = new Dictionary<string, string>
+                                {
+                                    {"Logging:Elasticsearch:IncludeScopes", "true" },
+                                    {"Logging:Elasticsearch:LogLevel:Default", "Error" },
+                                    {"Logging:Elasticsearch:LogLevel:My.Bananas.Trace", "Trace" },
+                                    {"Logging:LogLevel:Default", "Information" }
+                                }
+                            })
+                            .Build();
+
+            var serviceProvider = new ServiceCollection()
+                                        .AddLogging(x =>
+                                        {
+                                            x.AddConfiguration(config.GetSection("Logging"));
+                                            x.AddElasticSearch(options =>
+                                            {
+                                                options.ElasticsearchEndpoint = new Uri("http://bananas.net");
+                                                options.IndexName = "trace";
+                                            });
+                                        })
+                                        .BuildServiceProvider();
+
+            var loggerFilterOptions = serviceProvider.GetService<IOptions<LoggerFilterOptions>>();
+
+            ////This category doesn't exist in our diagnostics.json, so we should get the default (error)
+            //var retrievedLogLevel = loggerFilterOptions.GetLogLevelForCategoryName("My.Bananas.Trace");
+
+            //Assert.Equal(LogLevel.Trace, retrievedLogLevel);
+        }
+
+        [Fact]
+        public void No_elasticsearch_section_should_use_default_log_level()
+        {
+            var config = new ConfigurationBuilder()
+                            .Add(new MemoryConfigurationSource
+                            {
+                                InitialData = new Dictionary<string, string>
+                                {
+                                    {"Logging:LogLevel:Default", "Information" }
+                                }
+                            })
+                            .Build();
+
+            var serviceProvider = new ServiceCollection()
+                                        .AddLogging(x =>
+                                        {
+                                            x.AddConfiguration(config.GetSection("Logging"));
+                                            x.AddElasticSearch(options =>
+                                            {
+                                                options.ElasticsearchEndpoint = new Uri("http://bananas.net");
+                                                options.IndexName = "trace";
+                                            });
+                                        })
+                                        .BuildServiceProvider();
+
+            var loggerFilterOptions = serviceProvider.GetService<IOptions<LoggerFilterOptions>>();
+
+            ////This category doesn't exist in our diagnostics.json, so we should get the default (error)
+            //var retrievedLogLevel = loggerFilterOptions.GetLogLevelForCategoryName("My.Bananas.Trace");
+
+            //Assert.Equal(LogLevel.Information, retrievedLogLevel);
+        }
+
+        [Fact]
+        public void No_logging_config_should_use_builders_minimum_level()
+        {
+            var serviceProvider = new ServiceCollection()
+                                        .AddLogging(x =>
+                                        {
+                                            x.SetMinimumLevel(LogLevel.Critical);
+                                            x.AddElasticSearch(options =>
+                                            {
+                                                options.ElasticsearchEndpoint = new Uri("http://bananas.net");
+                                                options.IndexName = "trace";
+                                            });
+                                        })
+                                        .BuildServiceProvider();
+
+            var loggerFilterOptions = serviceProvider.GetService<IOptions<LoggerFilterOptions>>();
+
+            ////This category doesn't exist in our diagnostics.json, so we should get the default (error)
+            //var retrievedLogLevel = loggerFilterOptions.GetLogLevelForCategoryName("My.Bananas.Trace");
+
+            //Assert.Equal(LogLevel.Critical, retrievedLogLevel);
+        }
+    }
+}
