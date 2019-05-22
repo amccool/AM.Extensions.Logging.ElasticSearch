ca4846bddf38f2d4d84ded9d89eadb0ff78ceb9b
Alex McCool <alex.mccool@yahoo.com>
2019-05-22 13:06:11 -0700

    ES as a fixture (#18)
    * Adding .vscode to gitignore
    
    * Fixed bug with logger provider not recognizing config levels
    
    * Removing unused extension method
    
    * before moving to fixture
    
    * fixture of ES
    
    * full fixtures
    
    * incorrect assert

--- src/ElasticLogger.Test/ESLoggerTests.cs
+++ src/ElasticLogger.Test/ESLoggerTests.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 using Nest;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xunit;
 using Xunit.Abstractions;
 using LogLevel = Microsoft.Extensions.Logging.LogLevel;
@@ -28,60 +29,6 @@ namespace ElasticLogger.Test
         public ESLoggerTests(ITestOutputHelper outputHelper)
         {
             _output = outputHelper;
-        }
-
-        [Fact]
-        public async Task FullESWriteTest()
-        {
-            using (var elasticsearch = new ElasticsearchInside.Elasticsearch(c => c.SetElasticsearchStartTimeout(60)
-                .EnableLogging().LogTo(s=>_output.WriteLine(s??string.Empty))))
-            {
-                await elasticsearch.Ready();
-
-                ILoggerFactory loggerFactory = new LoggerFactory()
-                    .AddElasticSearch(elasticsearch.Url);
-
-                var logger = loggerFactory.CreateLogger("xxxxxxx");
-
-                var circularRefObj = new Circle();
-                circularRefObj.me = circularRefObj;
-
-                logger.Log(LogLevel.Critical, new EventId(), circularRefObj, null, (circle, exception) => "");
-
-                var delayTask = Task.Delay(TimeSpan.FromSeconds(2));
-                var client = new ElasticClient(new ConnectionSettings(elasticsearch.Url));
-                await client.PingAsync();
-                await delayTask;
-
-                var resp = await client.CatIndicesAsync();
-
-                Assert.Single(resp.Records);
-            }
-        }
-
-        [Fact]
-        public async Task FullESTest()
-        {
-            using (var elasticsearch = new ElasticsearchInside.Elasticsearch(c => c.SetElasticsearchStartTimeout(60)
-                .EnableLogging().LogTo(s => _output.WriteLine(s ?? string.Empty))))
-            {
-                await elasticsearch.Ready();
-                var client = new ElasticClient(new ConnectionSettings(elasticsearch.Url));
-                await client.PingAsync();
-
-                var tweet = new Tweet
-                {
-                    Id = 1,
-                    User = "kimchy",
-                    PostDate = new DateTime(2009, 11, 15),
-                    Message = "Trying out NEST, so far so good?"
-                };
-                var response = await client.IndexAsync(tweet, idx => idx.Index("mytweetindex"));
-                var response2 = client.Get<Tweet>(1, idx => idx.Index("mytweetindex"));
-                var tweetResp = response;
-                var tweetResp2 = response2.Source;
-                Assert.Equal(tweet, tweetResp2);
-            }
         }
 
 
