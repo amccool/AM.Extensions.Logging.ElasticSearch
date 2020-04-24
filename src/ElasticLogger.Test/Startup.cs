4f3acd32f6048e2eca69a2db9b0eec2d4f60707a
Alex McCool <Alex.McCool@yahoo.com>
2020-04-24 13:47:04 -0700

    hosting aspnetcore seems to be an issue

--- src/ElasticLogger.Test/Startup.cs
+++ src/ElasticLogger.Test/Startup.cs
@@ -14,7 +14,7 @@ namespace ElasticLogger.Test
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
